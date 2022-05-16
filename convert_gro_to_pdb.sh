2eca30b2efe9b7e96caa8da57551a8a1761ffd74
Hayden Dennison <dennisty@mail.uc.edu>
2022-05-16 16:06:24 -0400

    Added script to enter keystroke 2 to trjconv

--- convert_gro_to_pdb.sh
+++ convert_gro_to_pdb.sh
@@ -3,37 +3,46 @@
 
 STARTINGDIR="./"
 
-recursive_convert() {
-	for f in $STARTINGDIR
+convert() {
+	for f in ./*
 	do
-		echo "$f"
+		
 		if [ -d "$f" ]
 		then
-			cd "$f"
-			recursive_convert
-		fi
-		if [[ $f == *.gro ]]
+			(cd "$f";
+			convert)
+		elif [[ $f == *.gro ]]
 		then
 			echo "Converting $f to .pdb"
 			filename=$(basename $f .gro)
-			echo $filename
-			fullfilename="$filename.top"
-			gmx x2top -f $f -o fullfilename
+			#fullfilename="$filename.top"
+			#gmx x2top -f $f -o $fullfilename
 		
 			PDBFILENAME="$filename.pdb"
-			gmx trjconv -f $f -t A -o PDBFILENAME
+			#get entire system including H atoms
+			(echo 2 | gmx trjconv -f $f -s $f -o $PDBFILENAME)
 		fi
 	done
 }
 
-recursive_convert
+convert
 
 
-#find . -mindepth 1 -type f -name ".gro" -exec [
+#find . -mindepth 1 -type f -name ".gro" -exec (
 #	echo "Converting $f to .pdb";
-#	STRIPPEDNAME = ${x%.gro};
-#	TOPFILENAME = "$A.top";
+#	STRIPPEDNAME=${x%.gro};
+#	TOPFILENAME="$A.top";
 #	gmx x2top -f $f -o A;
 #
-#	PDBFILENAME = "$STRIPPEDNAME.pdb";
-#	gmx trjconv -f $f -t A -o PDBFILENAME;] {} +
+#	PDBFILENAME="$STRIPPEDNAME.pdb";
+#	gmx trjconv -f $f -t  -o PDBFILENAME;) {} +
+
+
+#find . -mindepth 1 -type f -name ".gro" -exec 
+#	echo "Converting $f to .pdb";
+#	STRIPPEDNAME=$(basename $f .gro)
+#	TOPFILENAME="$A.top";
+#	gmx x2top -f $f -o A;
+#
+#	PDBFILENAME="$STRIPPEDNAME.pdb";
+#	echo 2 | gmx trjconv -f $f -s $f -o PDBFILENAME; {} +
